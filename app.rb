64682dd5174a9a82499eaabcf9b60d30d0694586
Jordan Dale <Jordan.T.Dale@gmail.com>
2014-06-26 16:27:26 -0600

    the app file without a single exercise done

--- app.rb
+++ app.rb
@@ -1,17 +1,30 @@
 require "sinatra/base"
-require "sinatra/reloader"
+
 
 class MyApp < Sinatra::Application
-  register Sinatra::Reloader
 
   def initialize
     super
-    @items = []
+    @items = ["Jeff", "Graham", "Cam"]
   end
 
-  get "/" do
-    "items length: #{@items.length}"
+  get '/' do
+    erb :index, :locals => {:items => @items}
   end
 
-  run! if app_file == $0
-end
+  get '/items' do
+    "How many items are here? #{@items.length}"
+  end
+
+  get 'items/:name' do |n|
+    "Hello, #{n}!"
+  end
+
+  get "/items/:identifier" do
+    identifier = params[:identifier]
+    "you found item '#{@items[identifier.to_i]}' and its identifier is #{identifier.to_i + 1}"
+  end
+
+    run! if app_file == $0
+  end
+
